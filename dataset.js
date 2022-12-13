13cdf5beb4eedf97776704c12c22ac15f78e3e66
Hari Prakash <summithari@gmail.com>
2022-12-13 13:54:11 -0700

    updated ig

--- dataset.js
+++ dataset.js
@@ -1,4 +1,12 @@
-elment = document.querySelector('div');
-dataset = new Dataset(elment);
-// get data set "announce-page" from element
-dataset.get('announce-page', 'claro-standard-layout-page');
+// domain = 'https://summitlearning.org'
+
+var bodyFunction = document.getElementById("BODY")[0].getAttribute("onload");
+
+// CHECK PAGE NAME AND SET VARIABLES
+if (window.location.pathname == '/my/announcements') {
+    var domain = 'https://summitlearning.org';
+    var page = 'announcements';
+    
+    // create data-url() within <body> tag
+    document.getElementById("BODY")[0].setAttribute("data-url", page);
+}
